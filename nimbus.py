a754933941c398ab58c37bc43274fac808660270
Tim Adams <adamstimb@gmail.com>
2019-11-20 21:24:27 +0100

    loads of stuff fixed and started keyboard input

--- nimbus.py
+++ nimbus.py
@@ -1,12 +1,19 @@
-from tools import logo, message, fatal, colour_to_bgr, font_image_selecta
+from tools import logo, message, fatal, colour_to_bgr, font_image_selecta, colrows_to_xy, plonk_image
 import cv2
 import numpy as np
 import time
 import threading
+import random
+import simpleaudio as sa
 from videostream import VideoStream
 from colour_table import colour_table, high_res_colour_table, low_res_default_colours, high_res_default_colours
 from command import Command
 from welcome import welcome
+
+
+# Floppy drive sounds: https://freesound.org/people/MrAuralization/packs/15891/
+
+default_cursor_image = np.ones((2, 10, 3), dtype=np.uint8) * 255
 
 
 class Nimbus:
@@ -16,6 +23,17 @@ class Nimbus:
     interface for your application.  
 
     """
+
+    def __onMouse(self, event, x, y, flags, param):
+        """Handle mouse and some keyboard events
+
+        """
+
+        if event == cv2.EVENT_FLAG_SHIFTKEY:
+            self.shift_pressed = True
+        else:
+            self.shift_pressed = False
+
 
     def __init__(self, full_screen=False, debug=False, title='Nimbusinator', border_size=40):
         """Create a new Nimbus object
@@ -54,6 +72,11 @@ class Nimbus:
         self.high_res_default_colours = high_res_default_colours.copy()
         self.low_res_colours = low_res_default_colours.copy()
         self.high_res_colours = high_res_default_colours.copy()
+        self.cursor_image = default_cursor_image.copy()
+        self.cursor_flash = False
+        self.show_cursor = False
+        self.floppy_is_running = False
+        self.shift_pressed = False
         self.vs = VideoStream(self.screen_size, queue_size=16).start()  # VideoStream object to display the Nimbus
 
 
@@ -123,6 +146,19 @@ class Nimbus:
         return self.vs.get_screen()
 
 
+    def __cycle_cursor_flash(self):
+        """Cycle the cursor flash
+
+        This needs to run in it's own thread
+
+        """
+        while self.running:
+            # When the Nimbus is boot flip cursor flash every 0.5 secs
+            time.sleep(0.5)
+            cursor_flash = self.cursor_flash
+            self.cursor_flash = not cursor_flash
+
+
     def __render_display(self, screen_data):
         """Generate final display data including border
 
@@ -146,13 +182,27 @@ class Nimbus:
         # Make the display image as an empty array then add the border colour
         display_data = np.zeros((vertical_display_length, horizontal_display_length, 3), dtype=np.uint8)
         cv2.rectangle(display_data, (0,0), (horizontal_display_length, vertical_display_length), colour_to_bgr(self, self.border_colour), -1)
-        # resize the screen_data and add it to display
-        resized = cv2.resize(screen_data, (640, 500), interpolation=cv2.INTER_LINEAR_EXACT)
+        # Add cursor if showing cursor
+        final_screen_data = screen_data.copy()
+        if self.show_cursor:
+            coord = colrows_to_xy(self.screen_size, self.cursor_position)
+            # Only overlay cursor if on screen and cursor flash is true
+            if coord[0] < self.screen_size[0] and self.cursor_flash:
+                final_screen_data = plonk_image(self, screen_data, self.cursor_image, coord)
+        # resize the combined screen_data and add it to display
+        resized = cv2.resize(final_screen_data, (640, 500), interpolation=cv2.INTER_LINEAR_EXACT)
         display_data[self.border_size:self.border_size+resized.shape[0], self.border_size:self.border_size+resized.shape[1]] = resized
         return display_data
 
 
-    def __runner(self):
+    def __handle_keypress_event(self, key):
+        """Add a keypress to the bufferr
+
+        """
+        message('{}')
+
+
+    def __screen_runner(self):
         """Display the Nimbus in a window
 
         This function contains a loop which can be broken by setting
@@ -167,12 +217,66 @@ class Nimbus:
                 message('Full screen mode')
             cv2.namedWindow(self.title, cv2.WND_PROP_FULLSCREEN)
             cv2.setWindowProperty(self.title, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        cv2.namedWindow(self.title)
+        cv2.setMouseCallback(self.title, self.__onMouse)
         while self.running:
             frame = self.__render_display(self.vs.get_screen())
             cv2.imshow(self.title, frame)
             key_pressed = cv2.waitKey(5)
+            # Handle key presses
+            if key_pressed != -1:
+                self.__handle_keypress_event(key_pressed)
         if self.debug:
             message('Display loop stopped')
+
+
+    def __floppy_drive_effects(self):
+        """Simulate floppy drive grinding
+
+        This has to be run a thread.  Two drive sounds are supplied: a
+        short 'dot' sounds, and a long 'dash' sound, with 4 variations
+        of each.  Semi-random patterns of dashes and dots are generated 
+        with randomly-selected variations while the floppy_is_running
+        flag is True.
+
+        """
+        while self.running:
+            # Load sound effects
+            dash = []
+            dot = []
+            for i in range(1, 5):
+                dash.append(sa.WaveObject.from_wave_file('data/floppy-dash{}.wav'.format(i)))
+                dot.append(sa.WaveObject.from_wave_file('data/floppy-dot{}.wav'.format(i)))
+            # If flag is True play a bunch of grinding floppy drive sounds
+            if self.floppy_is_running:
+                # play dash (pick one at random)
+                play_obj = dash[random.randint(0, 3)].play()
+                play_obj.wait_done()
+                # play random number of dots
+                for i in range(0, random.randint(1, 5)):
+                    # break out if drive is stopped
+                    if not self.floppy_is_running:
+                        return
+                    else:
+                        # pick one at random
+                        play_obj = dot[random.randint(0, 3)].play()
+                        play_obj.wait_done()
+
+
+    def run_floppy(self, flag):
+        """Run or stop the floppy drive sound effects
+
+        Augment your user experience with the industrial melodies of a 
+        1980s PC floppy drive
+
+        Args:
+            flag (boolean): True to run the drive, False to stop
+
+        """
+
+        if self.debug:
+            message('run floppy {}'.format(flag))
+        self.floppy_is_running = flag
 
 
     def boot(self, skip_welcome_screen=False):
@@ -186,12 +290,19 @@ class Nimbus:
 
         """
 
+        # otherwise go ahead
         message('Booting up')
         if self.debug:
             message('Dropping runner into a thread')
-        # Fire up runner in a thread
-        t = threading.Thread(target=self.__runner, args=())
-        t.start()
+        # Fire up screen runner in a thread
+        t_screen = threading.Thread(target=self.__screen_runner, args=())
+        t_screen.start()
+        # Fire up cursor in another thread
+        t_cursor = threading.Thread(target=self.__cycle_cursor_flash, args=())
+        t_cursor.start()
+        # Fire up the flopp disk effects in another
+        t_floppy = threading.Thread(target=self.__floppy_drive_effects, args=())
+        t_floppy.start()
         if skip_welcome_screen:
             # don't bother with loading screen
             message('Running')
@@ -199,13 +310,12 @@ class Nimbus:
         else:
             if self.debug:
                 message('Running Welcome Screen')
-            welcome(Command(self))
+            welcome(Command(self), self)
             if self.debug:
                 message('Welcome Screen finished')
             message('Running')
 
-
-    
+  
     def shutdown(self):
         """Shutdown the Nimbus
 
