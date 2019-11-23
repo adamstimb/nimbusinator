a31116d56cfe5e96ea47ee37e27f9b2db2849efa
Tim Adams <adamstimb@gmail.com>
2019-11-23 10:48:51 +0100

    update release number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('readme.md', 'r') as f:
 
 setuptools.setup(
     name="nimbusinator",
-    version="0.0.1",
+    version="0.0.2",
     author="Tim Adams",
     author_email="adamstimb@gmail.com",
     description="RM Nimbus GUI for Python",
